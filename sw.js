527cc7e05e42371bff124a8c0c7777f0718a6dbe
ikoo09 <nicodamara0909@gmail.com>
2026-06-24 18:42:04 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11179787
+    "domain": "3nbf4.com",
+    "zoneId": 11195266
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
