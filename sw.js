397c1f73254710a98efa55027fd8e1bc552266aa
ikoo09 <nicodamara0909@gmail.com>
2026-09-11 10:25:53 +0700

    fix: remove legacy third-party service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11195419
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+// CryptoScan Pro service worker cleanup.
+// Immediately unregister the legacy third-party worker and clear old caches.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    await self.registration.unregister();
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    const clients = await self.clients.matchAll({ type: 'window' });
+    clients.forEach(client => client.navigate(client.url));
+  })());
+});
